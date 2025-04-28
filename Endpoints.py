e4f879202a01c9617a0d2b3a42b3f98fa1a47405
MOHAMED EL AFIA <mohamedelafia723@gmail.com>
2025-04-28 17:58:37 +0100

    update

--- Endpoints.py
+++ Endpoints.py
@@ -7,11 +7,13 @@ from typing import List
 from keras.models import load_model
 from pydantic import BaseModel 
 from fastapi.middleware.cors import CORSMiddleware 
+import google.generativeai as genai 
+
 
 app = FastAPI()
 
 origins = [
-    "https://medelafia.github.io",
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
@@ -35,7 +37,10 @@ disease_blood_model = None
 ## definition of disease
 df_medquad = None 
 df_labels = None 
+## definition of geneartive ai model 
 
+generativeai_model = None 
+ 
 def replace_space(x) : 
     return re.sub(r'\(.*?\)' , '' , x).lower().strip().lstrip().rstrip().replace(" " , "_") 
 
@@ -53,6 +58,8 @@ async def start_up() :
 
     global df_medquad
     global df_labels
+
+    global generativeai_model 
     ## simple dieseae data prepare
     df_disease_train = pd.read_csv("Training.csv") 
     df_disease_test = pd.read_csv("Testing.csv") 
@@ -79,12 +86,15 @@ async def start_up() :
 
     df_labels = pd.merge( df_labels_defintions , df_labels_pre, on="Disease")
     df_labels["Disease"] = df_labels["Disease"].apply(replace_space)
-    print(df_labels["Disease"])
+    
+    genai.configure(api_key="AIzaSyB2SICCajUAwD6pI9WzRObKGgRcg3wN5qs") 
+    generativeai_model = genai.GenerativeModel(model_name="gemini-1.5-flash") 
 
 class BloodFeatures(BaseModel) : 
     name : str 
     value : float 
 
+## endpoints for the disease predection 
 @app.get("/predictDisease")
 async def predict_disease(symptoms : str) : 
     new_row = {col : 0 for col in df_disease_columns }  
@@ -100,15 +110,24 @@ async def predict_disease(symptoms : str) :
                 result["causes"] = df_medquad["answer"].iloc[i]
     else : 
         result["causes"] = None 
+    ## search for the disease information in the list of diseases 
     data_row = df_labels.where(df_labels["Disease"] == name).dropna()
     if data_row.size >= 1 : 
         result['definition'] = data_row.iloc[0]["Description"]
         result["precaution"] = "you have to " + " and ".join(data_row.iloc[0].values[2:].tolist())
+    else : 
+        ## get the data from google gemini api 
+        precaution = generativeai_model.generate_content(f"what's the precautions of {name} , give me just the precautions")
+        definition = generativeai_model.generate_content(f"what's {name} , give me just the definition") 
+        result['definition'] = definition.text 
+        result["precaution"] = precaution.text
     return result
     
+## endpoints to get all diseases symptoms 
 @app.get("/getSymptoms") 
 async def get_symptoms() : 
     return { "symptoms" : df_disease_columns.tolist()} 
+
 @app.post("/predictBloodDisease")
 async def predict_blood_disease(blood_features : List[BloodFeatures]) : 
     new_row = { col : 0.0 for col in df_disease_blood_columns }
@@ -125,8 +144,10 @@ async def predict_blood_disease(blood_features : List[BloodFeatures]) :
             elif df_medquad["question"].iloc[i].find("causes") : 
                 result["causes"] = df_medquad["answer"].iloc[i]
     else : 
-        result["causes"] = None 
-        result["definition"] = None
+        causes = generativeai_model.generate_content(f"what's the causes of {name} , give me just the causes")
+        definition = generativeai_model.generate_content(f"what's {name} , give me just the definition")  
+        result["causes"] = causes.text 
+        result["definition"] = definition.text
     return result 
 
 @app.get("/getBloodFeatures") 
